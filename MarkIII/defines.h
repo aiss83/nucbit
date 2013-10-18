db86925b60ee70aca368858cffa72639c32611c4
Myrex <myrex@deep-one.in>
2013-10-18 15:36:44 +0600

    Base version as eclipse-based project

---
+++ MarkIII/defines.h
@@ -0,0 +1,24 @@
+/*
+ * defines.h
+ *
+ *  Created on: 18.10.2013
+ *      Author: 17095
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define CONTIKI 1
+#define CONTIKI_TARGET_MB851 1
+
+#define PLATFORM_HEADER "hal/micro/cortexm3/compiler/gnu.h"
+#define BOARD_HEADER "board.h"
+
+#define CORTEXM3
+#define CORTEXM3_STM32W108
+#define PHY_STM32W108XX
+#define DISABLE_WATCHDOG
+
+#define AUTOSTART_ENABLE 1
+
+#endif /* DEFINES_H_ */
