451eae9005551eb496c48e28f3df2cf773c52f31
Myrex <Myrex@oasup.pnppk.ru>
2014-09-04 15:54:06 +0600

    Finished base prototype for flash driver

--- MarkIII/platform/NucBee/dev/spi_flash.c
+++ MarkIII/platform/NucBee/dev/spi_flash.c
@@ -5,13 +5,58 @@
  *      Author: 17095
  */
 
-
 #include "spi.h"
 #include "spi_flash.h"
 
-int spi_flash_init(){
+int spi_flash_init() {
+	int ret = 0;
 
+	return ret;
+}
 
+static inline int fl_makeaddr(int adr) {
+	int ret = 0;
 
+	int pagen, shift;
 
+	pagen = adr % 512;
+
+	shift = adr - (pagen * 512);
+
+	//fine shift
+	ret = (shift & 0x3ff) << 12 | (pagen & 0xfff);
+
+	return ret;
+}
+
+int spi_flash_read(char *data, int adr, int size) {
+	spi2_enable_cs();
+
+	int tmp = CMD_CNTREAD_HF;
+	spi2_write((char*) &tmp, 1);
+
+	tmp = fl_makeaddr(adr);
+	spi2_write((char*) &tmp, 4); // with extra dummy-byte
+
+	tmp = spi2_read(data, size);
+
+	spi2_disable_cs();
+
+	return tmp;
+}
+
+int spi_flash_write(char *data, int adr, int size) {
+	spi2_enable_cs();
+
+	int tmp = CMD_MAIN_READ;
+	spi2_write((char*) &tmp, 1);
+
+	tmp = fl_makeaddr(adr);
+	spi2_write((char*) &tmp, 4); // with extra dummy-byte
+
+	tmp = spi2_write(data, size);
+
+	spi2_disable_cs();
+
+	return tmp;
 }
